41e09ca7d233a9207f84f6e109aa16676e48cad6
zingwena <zingwenawarren1997@gmail.com>
2022-04-01 08:35:47 +0200

    first commit

--- lib/pages/home.dart
+++ lib/pages/home.dart
@@ -1,143 +1,103 @@
-import 'package:africanscity/pages/profile.dart';
-import 'package:africanscity/pages/settings.dart';
 import 'package:flutter/material.dart';
+import 'package:flutter/widgets.dart';
 
-import 'contacts.dart';
-import 'messages.dart';
 class MyHomePage extends StatefulWidget {
-MyHomePage({Key? key, required this.title}) : super(key: key);
-final String title;
-
-@override
-_MyHomePageState createState() => _MyHomePageState();
+  MyHomePage({Key? key,}) : super(key: key);
+Widget customSearchBar=const Text('Africans city');
+  @override
+  _MyHomePageState createState() => _MyHomePageState();
 }
 
 class _MyHomePageState extends State<MyHomePage> {
-  int currentIndex=0;
-  final screens=[
-    Center(child:Text('Home',style:TextStyle(fontSize: 50))),
-    Center(child:Text('Delete',style:TextStyle(fontSize: 50))),
-    Center(child:Text('Feed',style:TextStyle(fontSize: 50))),
-  ];
-  @override
+  Icon customIcon=const Icon(Icons.search);
   Widget build(BuildContext context) {
     return Scaffold(
-        backgroundColor:Colors.greenAccent,
-        appBar: AppBar(
-          backgroundColor:Colors.lightGreenAccent,
-          title: Text("Notes"),
-          actions: <Widget>[
-            IconButton(
-              icon: Icon(Icons.delete),
+      appBar: AppBar(
 
-              tooltip: 'Delete Icon',
-              onPressed: () {},
-            ), //IconButton
-            IconButton(
-              icon: Icon(Icons.settings),
-              tooltip: 'Setting Icon',
-              onPressed: () {},
-            ), //IconButton
+        title: Text('Africans City'),
+        actions: <Widget>[
+        IconButton(
+          icon: Icon(Icons.search),
+
+            onPressed: (){
+              setState(() {
+                if(customIcon.icon==Icons.search){
+                  customIcon=const Icon(Icons.cancel);
+                  customSearchBar=const ListTile(
+                    leading: Icon(Icons.search,color: Colors.white,size:28),
+                  );
+
+        TextField(
+    decoration:InputDecoration(
+    hintText:'type in....',
+    hintStyle:TextStyle(color:Colors.white,fontSize:18,fontStyle:FontStyle.normal)
+    ),
+        );
+    TextStyle(color:Colors.white);
+                }
+              });
+            },
+
+          ),
+
+
+
+        ],
+        backgroundColor: Colors.orange,
+      ),
+      body: Container(
+
+      ),
+      drawer: Drawer(
+    child: ListView(
+
+        padding:EdgeInsets.zero,
+      children: [
+        SizedBox(height: 40,),
+            const DrawerHeader(
+              child: Icon(Icons.person_outlined, color: Colors.grey, size: 100),
+
+              decoration:BoxDecoration(
+                color: Colors.orange,
+              ),
+
+
+            ),
+           ListTile(
+             title: Text('Featured Listing'),
+
+             onTap: (){
+
+             },
+           ),
+            ListTile(
+              title:Text ('About'),
+              onTap: (){
+
+              },
+            ),
+            ListTile(
+              title:Text('Properties'),
+              onTap: (){
+
+              },
+            ),
+            ListTile(
+              title: Text('Contacts'),
+              onTap: (){
+
+              },
+            ),
+            ListTile(
+              title: Text('Pricing'),
+              onTap: (){
+
+              },
+            ),
           ],
-          //<Widget>[]
+        ),
+      ),
 
-          elevation: 50.0,
-          brightness: Brightness.dark,
-
-        ), //AppBar
-        drawer: Drawer(
-            child: ListView(
-                padding: EdgeInsets.zero,
-                children: <Widget>[
-                  DrawerHeader(
-                      decoration: BoxDecoration(
-                        color: Colors.green,
-                      ),
-                      child: Text(
-                        'DrawerHeader',
-                        style: TextStyle(
-                          color: Colors.black,
-                          fontSize: 24,
-                        ),
-                      )
-                  ),
-                  ListTile(
-                      leading: Icon(Icons.message),
-                      title: Text('messages'),
-
-                      onTap: () {
-                        Navigator.push(
-                          context,
-                          MaterialPageRoute(
-                              builder: (context) => const Messages()),
-                        );
-                      }
-
-                  ),
-                  ListTile(
-                      leading: Icon(Icons.account_circle),
-                      title: Text('Profile'),
-                      onTap: () {
-                        Navigator.push(
-                          context,
-                          MaterialPageRoute(
-                              builder: (context) => const Profile()),
-                        );
-                      }
-
-                  ),
-                  ListTile(
-                      leading: Icon(Icons.settings),
-                      title: Text('Settings'),
-                      onTap: (){
-                        Navigator.push(
-                          context,
-                          MaterialPageRoute(
-
-                              builder:(context)=>const Settings()),
-                        );
-                      }
-                  ),
-                  ListTile(
-                      leading: Icon(Icons.contacts),
-                      title: Text('Contacts'),
-                      onTap: (){
-                        Navigator.push(
-                          context,
-                          MaterialPageRoute(
-
-                              builder: (context)=>const Contacts()),
-                        );
-                      }
-                  ),
-
-                ])),
-        body: screens[currentIndex],
-        bottomNavigationBar: BottomNavigationBar(
-            currentIndex:currentIndex,
-            onTap: (index)=>setState(()=>currentIndex=index),
-            items:[
-              BottomNavigationBarItem(
-                icon:Icon(Icons.home),
-                label:'Homes',
-                backgroundColor: Colors.grey,
-
-              ),
-              BottomNavigationBarItem(
-                icon:Icon(Icons.delete),
-                label: 'Delete',
-                backgroundColor: Colors.grey,
-              ),
-              BottomNavigationBarItem(
-                icon:Icon(Icons.favorite),
-                label:'Feed',
-                backgroundColor: Colors.grey,
-              ),
-
-            ]
-        )
     );
   }
-
-
 }
